e929d6e923892c0cc4a453a7167c404f05af52db
Matt Sárdi <sardi.matt@gmail.com>
2023-02-25 23:44:03 +0100

    feat(backend): progress with cleaning

--- backend/backend/scraping/clean_html.py
+++ backend/backend/scraping/clean_html.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from pyprojroot import here
+from tqdm.auto import tqdm
 
 from backend.scraping.scrape_data import RAW_OUTPUT_DIR
 
@@ -30,6 +31,16 @@ def clean_one_page(local_filename: str):
     outfile.write_text(text)
 
 
+def clean_all():
+    for file in tqdm(list(RAW_OUTPUT_DIR.glob("*"))):
+        text = file.read_text()
+
+        text = clean_html(text)
+
+        outfile = here("../data/scraped/cleaned_all") / f"{file.stem}.txt"
+        outfile.write_text(text)
+
+
 def clean_html(html: str):
     soup = BeautifulSoup(html, "html.parser")
     div = soup.select_one(MAIN_CONTENT_CSS_SELECTOR)
@@ -40,7 +51,7 @@ def clean_html(html: str):
 
     # Add extra line breaks.
     for e in div.find_all("span", class_="jhId"):
-        e.string = "\n"
+        e.string = "\n<note>jhId=" + e["id"] + "</note>\n"
     # Beginning of paragraph should be on margin, so points under it are identifiable.
     for e in div.find_all("div", class_="bekezdesNyito"):
         if e.text[0] == " ":
@@ -58,6 +69,15 @@ def clean_html(html: str):
 
 
 if __name__ == "__main__":
-    url = "https://njt.hu/jogszabaly/1975-1-20-24"
+    pass
+    # url = "https://njt.hu/jogszabaly/1975-1-20-24"
     # url = "https://njt.hu/jogszabaly/2013-5-00-00"
-    scrape_one_page(url)
+    # url = "https://njt.hu/jogszabaly/2013-5-00-00"
+    # scrape_one_page(url)
+    # clean_one_page("1999-6-20-0A.html")
+
+    # clean_one_page("2002-35-00-00.html")
+    # clean_one_page(random.choice(list(RAW_OUTPUT_DIR.glob("*"))).name)
+    
+    # clean_all()
+
